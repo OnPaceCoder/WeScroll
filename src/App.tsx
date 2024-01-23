07ecd9f751d703d56df6b960006acd727a42ac49
Priyank Vasoya <priyankvasoya7@gmail.com>
2024-01-23 21:35:12 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+import { Routes, Route } from "react-router-dom";
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/sign-in" element={<SigninForm />} />
+        {/* Private Routes */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
