ba8087f413c3560cfce9ea2c85d4db1386f31173
Priyank Vasoya <priyankvasoya7@gmail.com>
2024-01-25 23:37:58 +0530

    Add: Loader created

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,15 @@
+const Loader = () => {
+  return (
+    <div>
+      <img
+        src="/assets/icons/loader.svg"
+        alt="loader"
+        width={24}
+        height={24}
+        className="animate-spin"
+      />
+    </div>
+  );
+};
+
+export default Loader;
