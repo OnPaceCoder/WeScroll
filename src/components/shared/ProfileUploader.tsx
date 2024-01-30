6edce196672f8197e9e4ae82ef3900760b177dd3
Priyank Vasoya <priyankvasoya7@gmail.com>
2024-01-30 23:50:12 +0530

    Add: Profile Uploader created

---
+++ src/components/shared/ProfileUploader.tsx
@@ -0,0 +1,20 @@
+const ProfileUploader = () => {
+  return (
+    <div>
+      <input className="cursor-pointer" />
+
+      <div>
+        <img
+          src={fileUrl || "/assets/icons/profile-placeholder.svg"}
+          alt="image"
+          className="h-24 w-24 rounded-full object-cover object-top"
+        />
+        <p className="text-primary-500 small-regular md:base-semibold">
+          Change profile photo
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileUploader;
