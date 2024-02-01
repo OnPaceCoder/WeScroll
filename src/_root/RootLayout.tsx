1e1b6d503870e15a186000b38f45cf1b49cb4aed
Priyank Vasoya <priyankvasoya7@gmail.com>
2024-02-01 23:44:24 +0530

    Add: Top, bottom and left side bar added

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,11 +1,15 @@
+import { Bottombar, LeftSidebar, Topbar } from "@/components/shared";
 import { Outlet } from "react-router";
 
 const RootLayout = () => {
   return (
     <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
       <section className="flex flex-1 h-full">
         <Outlet />
       </section>
+      <Bottombar />
     </div>
   );
 };
